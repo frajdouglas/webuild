41eceb960a2127094e8bbf376fcab73839c7edb2
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-08 10:18:27 +0000

    comments out test api url"

--- services/postEvent.ts
+++ services/postEvent.ts
@@ -8,7 +8,8 @@ export async function postEvent(eventData: {
 }) {
   try {
     const response = await axios.post(
-      "https://reimagined-giggle-xqr6pxwq4v9hr7j-3000.app.github.dev/calendar/event",
+      // "https://reimagined-giggle-xqr6pxwq4v9hr7j-3000.app.github.dev/calendar/event",
+      "https://calendarbook.onrender.com/calendar/event",
       eventData
     );
     return response.data;
